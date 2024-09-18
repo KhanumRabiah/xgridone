ee994a926f701c6f9a994cd53d810e96c7b8c795
RABIAH KHANUM <rabiah.khanum@xgrid.com>
2024-09-18 14:18:15 +0500

    Initial commit

---
+++ conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'xgridone'
+copyright = '2024, RabiahK'
+author = 'RabiahK'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+root_doc = 'root'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'classic'
+html_static_path = ['_static']
+
+html_context = {
+    'prev_page': 'Previous topic',  # Customize Previous button text
+    'next_page': 'Next topic',      # Customize Next button text
+}
