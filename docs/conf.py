6e88c117b847a4279b7dc65ce102d150ec6b1fda
KhanumRabiah <khanumrabiah@gmail.com>
2025-03-24 13:46:19 +0500

    Modification-7

--- docs/conf.py
+++ docs/conf.py
@@ -27,24 +27,3 @@ root_doc = 'index'
 import sphinx_rtd_theme
 
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-html_theme_options = {
-    "bodyfont": "Times New Roman",
-    "headfont": "Times New Roman",
-    "linkcolor":"#4169E1",
-    "visitedlinkcolor": "#B2BEB5",
-    "bgcolor": "#F0FFFF",
-    "headbgcolor": "#CCCCFF",
-    "sidebarbgcolor": "#A7C7E7",
-    "headtextcolor": "#301934",
-    "sidebartextcolor": "191970",
-    "sidebarlinkcolor": "#191970"
-}
-
-html_static_path = ['_static']
-
-html_context = {
-    'prev_page': 'Previous topic',  # Customize Previous button text
-    'next_page': 'Next topic',      # Customize Next button text
-}
